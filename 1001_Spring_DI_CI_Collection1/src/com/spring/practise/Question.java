e409c52ffd15ff01d2d73bd92c1cfd1b6af21c44
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-04-14 01:57:47 +0530

    DI using CI with collection

---
+++ 1001_Spring_DI_CI_Collection1/src/com/spring/practise/Question.java
@@ -0,0 +1,37 @@
+package com.spring.practise;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class Question 
+{
+    private int id;
+    String ques;   
+    private List<String> answer;
+		
+	public Question(int id,String ques,List<String> answer)
+	{   
+		super();
+		this.id = id;
+		this.ques = ques;
+		this.answer = answer;
+	}
+
+	//@Override
+	public void displayInfo() 
+	{
+		System.out.println("\nQuestion [id=" + id + ", ques=" + ques + "]");
+		
+		System.out.println("\nAnswers are :-");
+		
+		Iterator <String> itr = answer.iterator();
+		while(itr.hasNext())
+		{
+			System.out.println(itr.next());
+		}
+		
+	}
+	
+	
+
+}
