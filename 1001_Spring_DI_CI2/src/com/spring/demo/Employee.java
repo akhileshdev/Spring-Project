cf06bd78fe7eab9f83f97dbcd9dc9e0f70214659
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-04-13 19:06:43 +0530

    DI using CI with Primitive value

--- 1001_Spring_DI_CI2/src/com/spring/demo/Employee.java
+++ 1001_Spring_DI_CI2/src/com/spring/demo/Employee.java
@@ -22,7 +22,7 @@ public class Employee
 	@Override
 	public String toString() 
 	{
-		return "Employee [id=" + id + ", name=" + name + "]";
+		return "Employee [Id=" + id + ", Name=" + name + "]";
 	}
 
 	
