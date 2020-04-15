18d410e24913a402642113d591aca03f0af12784
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-04-16 00:22:31 +0530

    DI with Constructor using MAP as collection

---
+++ 1001_Spring_DI_CI_Collection3/src/com/spring/demo/Question.java
@@ -0,0 +1,41 @@
+package com.spring.demo;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class Question 
+{
+    private int id;
+    private String name;    
+    private Map<String,String> answer;
+        
+	public Question(int id,String name,Map<String,String> answer) 
+	{
+		this.id  = id;
+		this.name = name;
+		this.answer = answer;		
+	}
+	
+	public void displayInfo()
+	{		
+		System.out.println("\nQuestion : [ ID : "+id+" Ques : "+name+" ]");
+		
+		Set<Entry<String,String>> set = answer.entrySet();		
+		Iterator<Entry<String,String>>  itr = set.iterator();
+	
+		
+		while(itr.hasNext())
+		{
+			Entry<String,String> entry = itr.next();
+			
+			System.out.print("Answer : "+entry.getKey());
+			System.out.println("  Given By : "+entry.getValue());
+					
+		}
+		
+	}
+	
+
+}
---
+++ 1001_Spring_DI_CI_Collection3/src/com/spring/demo/Test.java
@@ -0,0 +1,19 @@
+package com.spring.demo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test 
+{
+
+	public static void main(String[] args) 
+	{
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		Question ques = (Question)context.getBean("ques");
+		
+		ques.displayInfo();
+
+	}
+
+}
