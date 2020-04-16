3dee1cf6975eaa097f5c21a10590062ed646f09a
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-04-17 03:17:34 +0530

    DI using SI with List<String> as Collection

---
+++ 1001_Spring_DI_SI_Collection2/src/com/spring/demo/Question.java
@@ -0,0 +1,52 @@
+package com.spring.demo;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class Question 
+{
+	private int id;
+	private String question;
+	private List<String> answer;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getQuestion() 
+	{
+		return question;
+	}
+
+	public void setQuestion(String question) 
+	{
+		this.question = question;
+	}
+
+	public List<String> getAnswer() 
+	{
+		return answer;
+	}
+
+	public void setAnswer(List<String> answer) 
+	{
+		this.answer = answer;
+	}
+
+	public void displayInfo() 
+	{
+        System.out.println("\nQuestion : [ID : "+id+" Question : "+question+" ]");
+        
+        Iterator<String> itr = answer.iterator();
+        
+        while(itr.hasNext())
+        {
+        	System.out.println(itr.next());
+        }
+	}
+
+}
