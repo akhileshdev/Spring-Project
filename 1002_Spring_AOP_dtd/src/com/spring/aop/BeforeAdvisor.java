a63d1c97e39c3bae77555e914aa687764673517b
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-04-21 02:18:27 +0530

    spring AOP with dtd

--- 1002_Spring_AOP_dtd/src/com/spring/aop/BeforeAdvisor.java
+++ 1002_Spring_AOP_dtd/src/com/spring/aop/BeforeAdvisor.java
@@ -3,11 +3,13 @@ package com.spring.aop;
 import java.lang.reflect.Method;
 import org.springframework.aop.MethodBeforeAdvice;
 
+//this class is Aspect , heart of spring AOP
+
 public class BeforeAdvisor implements MethodBeforeAdvice
 {
     
 	@Override
-	public void before(Method method,Object args[],Object target) throws Throwable
+	public void before(Method method,Object args[],Object target) throws Throwable  //Advice
 	{
 		System.out.println("Additional concern before Actual Logic");
 	}
