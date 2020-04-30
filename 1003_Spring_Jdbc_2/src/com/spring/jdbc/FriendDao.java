1e55fb427d56b93e35df824d13835436c8f960ab
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-05-01 04:17:52 +0530

    Spring JdbcTemplate implementation2

---
+++ 1003_Spring_Jdbc_2/src/com/spring/jdbc/FriendDao.java
@@ -0,0 +1,40 @@
+package com.spring.jdbc;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class FriendDao 
+{
+    private JdbcTemplate jdbcTemplate;
+    	
+	public JdbcTemplate getJdbcTemplate() 
+	{
+		return jdbcTemplate;
+	}
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) 
+	{
+		this.jdbcTemplate = jdbcTemplate;
+	}
+	
+	
+	public int saveFriend(Friend f)
+	{
+		String query = "insert into friend values('"+f.getId()+"','"+f.getName()+"','"+f.getAge()+"')";		
+		return jdbcTemplate.update(query);
+	}
+	
+	public int updateFriend(Friend f)
+	{
+		String query = "update friend set age = '"+f.getAge()+"' where id = '"+f.getId()+"'";
+		
+		return jdbcTemplate.update(query);
+	}
+	
+	public int deleteFriend(Friend f)
+	{
+		String query = "delete from friend where id = '"+f.getId()+"'";
+		
+		return jdbcTemplate.update(query);
+	}
+
+}
---
+++ 1003_Spring_Jdbc_2/src/com/spring/jdbc/Test.java
@@ -0,0 +1,26 @@
+package com.spring.jdbc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test 
+{
+    public static void main(String args[])
+    {
+    	ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+    	
+    	FriendDao fr = (FriendDao)context.getBean("fdao");
+    	
+        //int status = fr.deleteFriend(new Friend(401,"Rohan baba",23));
+    	
+    	
+    	Friend f = new Friend();
+    	f.setId(101);
+        
+    	int status = fr.deleteFriend(f); 
+    	
+        System.out.println("Status of Query : "+status);
+    	
+    }
+
+}
