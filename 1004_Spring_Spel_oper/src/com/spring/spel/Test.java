5a7191a9d3f370d3b7f7f453b1cfd40ebe8894c2
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-05-03 23:43:39 +0530

    Spring Spel with Operator

---
+++ 1004_Spring_Spel_oper/src/com/spring/spel/Test.java
@@ -0,0 +1,25 @@
+package com.spring.spel;
+
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+public class Test 
+{
+
+	public Test() 
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		ExpressionParser parser = new SpelExpressionParser();
+		
+		System.out.println("Parser : "+parser.parseExpression("'Welcome to '+'Spring Spel'").getValue());
+		
+
+	}
+
+}
