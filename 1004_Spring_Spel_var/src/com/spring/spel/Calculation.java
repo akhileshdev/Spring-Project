63570f37e4b79ebf72554c448c3ee4960e7f6b9c
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-05-03 23:25:13 +0530

    Spring Spel with variable

---
+++ 1004_Spring_Spel_var/src/com/spring/spel/Calculation.java
@@ -0,0 +1,23 @@
+package com.spring.spel;
+
+public class Calculation 
+{
+    private int number;
+	
+	public int getNumber() 
+	{
+		return number;
+	}
+	public void setNumber(int number) 
+	{
+		this.number = number;
+	}
+	
+	public int cube()
+	{
+		return number*number*number;
+	}
+	
+	
+
+}
