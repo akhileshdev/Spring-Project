430f1567f9af6daa10d2a28080df269f719cbe15
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-05-24 06:05:25 +0530

    MVC Form tag library with Dropdown List

--- 1006_Spring_Mvc_Form_dd/src/main/java/com/spring/mvc/controller/HelloController.java
+++ 1006_Spring_Mvc_Form_dd/src/main/java/com/spring/mvc/controller/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController
      public String reservationProcess(Model model)
      {   
 		 Reservation rs = new Reservation();  //Creating Reservation class Object to add it in model object
-		 model.addAttribute("reservation",rs);
+		 model.addAttribute("reservation",rs); //Adding bean class object 'res' to model attribute with name "reservation" 
     	 
 		 return "reservation-page.jsp"; //It will simply redirect to page reservation-page.jsp
      }
